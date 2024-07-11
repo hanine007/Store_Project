5f3d8b2d0322239e0597e1bc0de502b74eb100cd
hanine007 <bouchibahanine@gmail.com>
2024-07-11 23:22:52 +0100

    add route

---
+++ Backend/models/storemodel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const location = mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true,
+            trim: true // Supprime les espaces en début et en fin de chaîne
+         },
+         number :{
+                type:String,
+                required:true,
+                trim: true
+            
+         },
+         price :{
+            type:number,
+            required:true
+         },
+         entry_date :{
+            type:Date,
+            required:true
+         
+         },
+         exit_date :{
+            type:Date,
+            required:true
+         }
+
+    },
+    {
+        timestamps: true, // Ajoute createdAt et updatedAt automatiquement
+      }
+);
+export const Location =mongoose.model('Location',location)
+    
+  
