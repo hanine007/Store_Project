12da86ff6983f19e72084c1a26b305a3b6bca64f
hanine007 <bouchibahanine@gmail.com>
2024-07-12 02:05:18 +0100

    add update and get route

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -2,6 +2,7 @@ import { Location } from '../models/storemodel.js';
 import express from 'express';
 import { Router } from 'express';
 const route = Router();
+//post 
 route.post ('/app',async (req,res)=>{
     try{
         if(
@@ -18,16 +19,16 @@ route.post ('/app',async (req,res)=>{
         })
           }
 
-const newapp={
+    const newapp={
     name:req.body.name,
     number:req.body.number,
     price:req.body.price,
     entry_date:req.body.entry_date,
     exit_date:req.body.exit_date
 
-}
-const app = await Location.create(newapp)
-return res.status(200).send (app)
+    }
+    const app = await Location.create(newapp)
+    return res.status(200).send (newapp)
      
     }
     catch(error){
@@ -38,7 +39,71 @@ return res.status(200).send (app)
     }
     
 })
+//get all
+route.get('/app',async(req,res)=>{
+    try{
+        const apps= await Location.find({})
+        return  res.status(200).json({
+            count: apps.length,
+            data: apps
+        })
 
+    }
+  
+    catch(error){
+        console.log(error.message)
+        res.status(500).send({
+            message:error.message
+        })
+    }
 
+})
+//get one 
+route .get ('/app/:id',async(req,res)=>{
+    try {
+        const app = await Location.findById(req.params.id)
+        return res.status(200).json({
+            data:app
+        })
+    }
+    catch(error){
+        console.log(error.message)
+        res.status(500).send({
+            message:error.message
+        })
+    }
+})
+//update 
+route.put('/app/:id',async(req,res)=>{
+    try{
+        if (!req.body.name||
+            !req.body.number||
+            !req.body.price||
+            !req.body.entry_date||
+            !req.body.exit_date
+            )
+            {
+                return res.status(400).send({
+                    message:'All fields are required'
+                })
+            }
+            const result= await Location.findByIdAndUpdate(req.params.id,req.body)
+            if(!result){
+                return res.status(404).send({
+                    message:'App not found'
+                })
+            }
+            return res.status(200).send({
+                message:'App updated successfully'
+            })
+        
+    }
+    catch(error){
+        console.log(error.message)
+        res.status(500).send({
+            message:error.message
+        })
+    }
+})
 
 export default route;
