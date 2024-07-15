7d338057811cfc6c9d6c01717196e53a7a57679f
hanine007 <bouchibahanine@gmail.com>
2024-07-15 15:13:18 +0100

    add react router dom

--- Frontend/store/src/App.jsx
+++ Frontend/store/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="  text-center text-lg text-red-700 mt-4">
       Hello world!
     </h1>
   )
