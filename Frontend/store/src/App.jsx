bd7b4c57a26230fe520cf2830895711e10b77d65
hanine007 <bouchibahanine@gmail.com>
2024-07-15 15:39:32 +0100

    updates

--- Frontend/store/src/App.jsx
+++ Frontend/store/src/App.jsx
@@ -1,10 +1,15 @@
+//import React from "react";
 import { Route,Routes } from "react-router-dom";
+import {Show} from'./pages/Show'
+import {Home}from'./pages/Home'
 const app =()=>{
-  return
-  (
-<Routes>
-  <Route path='' element={}></Route>
-</Routes>
+  return (
+    
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Show" element={<Show />} />
+      </Routes>
   )
 }
 export default app
