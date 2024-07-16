c4426496cf43a9215385f09e6ec682f6e2a149bf
hanine007 <bouchibahanine@gmail.com>
2024-07-16 16:04:59 +0100

    add a edit

---
+++ Frontend/store/src/pages/Edit.jsx
@@ -0,0 +1,13 @@
+// src/pages/Edit.jsx
+
+
+export const Edit = () => {
+    // Contenu de votre composant Edit
+    return (
+        <div>
+            {/* Contenu de la page Edit */}
+        </div>
+    );
+};
+
+
