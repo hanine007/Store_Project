c1831de48e88a0660bed8cb2012c582b9c5f33e4
hanine007 <bouchibahanine@gmail.com>
2024-07-16 16:04:53 +0100

    add a delete

---
+++ Frontend/store/src/pages/Delete.jsx
@@ -0,0 +1,13 @@
+// src/pages/Delete.jsx
+
+
+
+export const Delete = () => {
+    // Contenu de votre composant Delete
+    return (
+        <div>
+            {/* Contenu de la page Delete */}
+        </div>
+    );
+};
+
