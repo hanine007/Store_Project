3a7464ce9bdec434ccb4d963942c4f2524cdf57a
hanine007 <bouchibahanine@gmail.com>
2024-07-16 16:04:35 +0100

    add routes

--- Frontend/store/src/App.jsx
+++ Frontend/store/src/App.jsx
@@ -4,12 +4,15 @@ import {Show} from'./pages/Show'
 import {Home}from'./pages/Home'
 import {Edit} from './pages/Edit'
 import {Delete}from'./pages/Delete'
+import {Create} from'./pages/Creat'
+
 const app =()=>{
   return (
     
       
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create/>}/>
         <Route path="/details/:id" element={<Show />} />
         <Route path="/edit/:id" element={<Edit />} />
         <Route path="/delete/:id" element={<Delete />} />
