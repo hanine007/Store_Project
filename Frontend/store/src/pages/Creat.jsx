f0c0de5a83a438a4f8f9d206281283558e237b7a
hanine007 <bouchibahanine@gmail.com>
2024-07-16 16:04:46 +0100

    create a route

---
+++ Frontend/store/src/pages/Creat.jsx
@@ -0,0 +1,14 @@
+// src/pages/Create.jsx
+
+
+
+export const Create = () => {
+    // Contenu de votre composant Create
+    return (
+        <div>
+            {/* Contenu de la page Create */}
+        </div>
+    );
+};
+
+
