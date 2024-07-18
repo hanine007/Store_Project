fdd0a71b07bf7049c1f637c8bf354c401167a53f
hanine007 <bouchibahanine@gmail.com>
2024-07-18 13:21:37 +0100

    add delete route

--- Frontend/store/src/pages/Delete.jsx
+++ Frontend/store/src/pages/Delete.jsx
@@ -1,12 +1,40 @@
-// src/pages/Delete.jsx
+import { useState } from "react";
+import { Spinner } from "../Components/spinner";
+import { Backto } from "../Components/Backto";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
 
 
 
 export const Delete = () => {
-    // Contenu de votre composant Delete
+const [loading,setLoading] = useState(false)
+const navigate = useNavigate();
+const {id}=useParams()
+const handleDELETE=()=>{
+
+    setLoading(true);
+    axios.delete(`http://localhost:3000/api/app/${id}`)
+    .then(()=>{
+    navigate('/')
+    setLoading(false);
+    })
+    .catch((error)=>{
+    setLoading(false)
+    console.log(error)
+    })
+}
     return (
-        <div>
-            {/* Contenu de la page Delete */}
+        <div className="p-4">
+            <Backto/>
+            <h1 className="text-3xl my-4">Delete Store</h1>
+            {loading?<Spinner/>:('') }
+            <div className="flex flex-col items-center border-2">
+                <h3 className="" >Are you sur you want to delete it ?</h3>
+                <button onClick={handleDELETE} className="bg-red-500 text-white p-2 rounded-md">Delete</button>
+             </div>
+
+            
         </div>
     );
 };
